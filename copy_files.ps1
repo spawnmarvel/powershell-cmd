05cc0d07f52fec9ec8cafd5d6484c200edb591ea
spawnmarvel <espenkleivane@gmail.com>
2017-02-15 09:49:58 +0100

    Update copy_files.ps1

--- copy_files.ps1
+++ copy_files.ps1
@@ -1,11 +1,11 @@
-copy-item C:\Users\jekl\Desktop\bat\powershell\source_folder\move.txt C:\Users\jekl\Desktop\bat\powershell\destination_folder
+copy-item C:\Users\me\Desktop\bat\powershell\source_folder\move.txt C:\Users\me\Desktop\bat\powershell\destination_folder
 $status = "The file was transferd and updated, successes."
 $date = get-date
 $log = $status + $date
 $run = "---------------------------- Script last run: "
 $lastrun = $run + $date
-Add-content -Path C:\Users\jekl\Desktop\bat\powershell\log.txt -Value $log
-Add-content -Path C:\Users\jekl\Desktop\bat\powershell\log.txt -Value $lastrun
+Add-content -Path C:\Users\me\Desktop\bat\powershell\log.txt -Value $log
+Add-content -Path C:\Users\me\Desktop\bat\powershell\log.txt -Value $lastrun
 
 
 #Bat = Powershell.exe -File C:\Users\jekl\Desktop\powershell\copy_command.ps1
