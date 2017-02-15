ef87be391c119c3fad869157d62224a0f40ec31e
spawnmarvel <espenkleivane@gmail.com>
2017-02-15 09:48:42 +0100

    Update create_many_folders.ps1

--- create_many_folders.ps1
+++ create_many_folders.ps1
@@ -4,14 +4,14 @@
 
 $fso = New-Object -ComObject scripting.filesystemobject
 
- for($i = 1;$i -le25;$i++) { $fso.CreateFolder("E:\AspenHistory_1\arcFolder\\arc0" + $i)}
+ for($i = 1;$i -le25;$i++) { $fso.CreateFolder("E:\His_1\arcFolder\\arc0" + $i)}
 
 
 $fso = New-Object -ComObject scripting.filesystemobject
 
- for($i = 1;$i -le99;$i++) { $fso.CreateFolder("C:\temp\folder\amib\arc0" + $i)}
+ for($i = 1;$i -le99;$i++) { $fso.CreateFolder("C:\temp\folder\amx\arc0" + $i)}
 
 
 $fso = New-Object -ComObject scripting.filesystemobject
 
- for($i = 1;$i -le11;$i++) { $fso.CreateFolder("E:\AspenHistory_1\TSK_DATA_HONE_" + $i)}
+ for($i = 1;$i -le11;$i++) { $fso.CreateFolder("E:\His_1\TSK_DATA_STORAGE_" + $i)}
