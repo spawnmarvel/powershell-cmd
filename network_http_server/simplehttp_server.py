454f89c718a231bd0d88f4472f41bb5552179430
spawnmarvel <espenkleivane@gmail.com>
2020-07-08 20:47:08 +0200

    changed http server

--- network_http_server/simplehttp_server.py
+++ network_http_server/simplehttp_server.py
@@ -4,9 +4,11 @@ import socketserver
 PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 
+# simplehttp_server.py in the same directory as index.html because by default the SimpleHTTPRequestHandler will look for a file named index.html in the current directory.
+# Passing an empty string as the ip address means that the server will be listening on any network interface (all available IP addresses).
 def simp_http():
     try:
-        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        with socketserver.TCPServer(("localhost", PORT), Handler) as httpd:
             print("serving at port", PORT)
             httpd.serve_forever()
     except KeyboardInterrupt as e:
