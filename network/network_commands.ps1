0ab54413b54c8040d462ed6ce64d5532cccb4b4a
spawnmarvel <espenkleivane@gmail.com>
2020-07-08 20:41:48 +0200

    changed http server

--- network/network_commands.ps1
+++ network/network_commands.ps1
@@ -3,3 +3,5 @@
 # # if we run the simple http server in python network_http_server, we get connected is true, if we stop the server we get an connection refused
 # check port and connection
 New-Object System.Net.Sockets.TcpClient("10......", 8080)
+Resolve-DnsName -Name "NO0117"
+Get-NetIPConfiguration
--- network_http_server/simplehttp_server.py
+++ network_http_server/simplehttp_server.py
@@ -4,7 +4,16 @@ import socketserver
 PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+def simp_http():
+    try:
+        with socketserver.TCPServer(("", PORT), Handler) as httpd:
+            print("serving at port", PORT)
+            httpd.serve_forever()
+    except KeyboardInterrupt as e:
+        print("closing" + str(e))
+        httpd.server_close()
+
+if __name__ == '__main__':
+    simp_http()
+
     
