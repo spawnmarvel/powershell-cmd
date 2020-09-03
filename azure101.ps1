30543ce66ac0d35cd6053321c67deca871fa7c3a
EspenK <espenkleivane@gmail.com>
2020-09-03 13:39:45 +0200

    azure

---
+++ azure101.ps1
@@ -0,0 +1,10 @@
+# Get all resources in the current subscription
+Get-AzResource | ft
+# Get all resources in a resource group
+Get-AzResource -ResourceGroupName bose-rg | ft
+# Get all resources whose resource group matches the provided wildcard
+Get-AzResource -ResourceGroupName bose* | ft
+# Get a resource group by wildcard
+Get-AzResourceGroup -Name ‘bose*’
+# Remove all resources in a group and the group
+Get-AzResourceGroup -Name ‘bose*’ | Remove-AzResourceGroup -Force -AsJob
