1b4807b068811b17a419970dca714122444ee936
EspenK <espenkleivane@gmail.com>
2020-09-08 08:38:13 +0200

    Update httpWebRequest.ps1

--- workish/httpWebRequest.ps1
+++ workish/httpWebRequest.ps1
@@ -3,16 +3,16 @@ function get_timestamp {
 }
 $stat = "Fail"
 Try {
-$url = "http://localhost:8080/AspenCoreSearch/"
-$req=[system.Net.HttpWebRequest]::Create($url);
-$res = $req.getresponse();
-$stat = $res.statuscode
-Add-Content http_check.txt "$(get_timestamp) : status :  $stat"
-$res.Close()
+    $url = "http://localhost:8080/AspenCoreSearch/"
+    $req=[system.Net.HttpWebRequest]::Create($url);
+    $res = $req.getresponse();
+    $stat = $res.statuscode
+    Add-Content http_check.txt "$(get_timestamp) : status :  $stat"
+    $res.Close()
 }
 Catch {
 
-#Add-Content http_check.txt "Could not find url " $(get_timestamp) 
-Add-Content http_check.txt "$(get_timestamp) : status :  $stat"
+    #Add-Content http_check.txt "Could not find url " $(get_timestamp) 
+    Add-Content http_check.txt "$(get_timestamp) : status :  $stat"
 
 }
