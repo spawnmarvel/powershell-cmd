e3f7b460b1a95992924d541b2bdbbf7be9c917a4
spawnmarvel <espenkleivane@gmail.com>
2020-12-05 16:48:36 +0100

    arm test template outpur for remove

--- arm/beginners_template/remove_rg.ps1
+++ arm/beginners_template/remove_rg.ps1
@@ -1,4 +1,6 @@
 # Remove all resources in a group and the group, this can take some time
-Get-AzResourceGroup -Name ‘boose*’ | Remove-AzResourceGroup -Force -AsJob
+$rv_info = Get-AzResourceGroup -Name ‘boose*’ | Remove-AzResourceGroup -Force -AsJob
+Write-Host $rv_info
 # Get all resources in a resource group
-Get-AzResource -ResourceGroupName boose-rg | ft
+$rv_result =  Get-AzResource -ResourceGroupName boose-rg | ft
+Write-Host $rv_result
