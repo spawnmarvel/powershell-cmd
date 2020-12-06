d052deb2a09690f4bb0fa85dd4b63c5f6cf5fa8f
spawnmarvel <espenkleivane@gmail.com>
2020-12-06 20:20:27 +0100

    add paramter

--- arm/beginners_template/first_template.ps1
+++ arm/beginners_template/first_template.ps1
@@ -2,5 +2,5 @@ $resourceGr = "boose-rg"
 # New rg
 New-AzResourceGroup -Name $resourceGr -Location "West Europe"
 # Deploy template
-# $templateFile = "C:\giti\powershell-cmd-bash\arm\beginners_template\azuredeploy.json"
-# New-AzResourceGroupDeployment -Name blanktemplate -ResourceGroupName $resourceGr -TemplateFile $templateFile
+$templateFile = "C:\giti\powershell-cmd-bash\arm\beginners_template\azuredeploy.json"
+New-AzResourceGroupDeployment -Name addName -ResourceGroupName $resourceGr -TemplateFile $templateFile -storageName "boosestorage1"
