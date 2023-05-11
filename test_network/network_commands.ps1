ee0d41ef3c57864f371c859c40029a876211ebdb
Espenk <espenkleivane@gmail.com>
2023-05-11 18:41:40 +0200

    edit repos

--- test_network/network_commands.ps1
+++ test_network/network_commands.ps1
@@ -1,6 +1,7 @@
 # https://docs.microsoft.com/en-us/powershell/module/nettcpip/test-netconnection?view=win10-ps
 
-# # if we run the simple http server in python network_http_server, we get connected is true, if we stop the server we get an connection refused
+# # if we run the simple http server in python network_http_server, 
+# we get connected is true, if we stop the server we get an connection refused
 # check port and connection
 New-Object System.Net.Sockets.TcpClient("10......", 8080)
 Resolve-DnsName -Name "NO0117"
